38e643bf1b588dec191263c5cfd830f78857329e
Gabicold <biel18102004@gmail.com>
2022-04-02 10:20:19 -0300

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class app extends React.Component{
+
+state = {
+  n1: null,
+  n2: null,
+  result: "O resultado aparecerá aqui",
+  sinal: ""
 }
 
-export default App;
+add = () => {
+  let {n1, n2} = this.state
+  if(n1 && n2 !== null){
+    this.setState({
+      result: n1 + n2,
+      sinal: "+"
+    })
+  } else{
+    this.setState({
+      result: "Voce precisa digitar os dois números!"
+    })
+  }
+}
+
+remove = () => {
+  let {n1, n2} = this.state
+  if(n1 && n2 !== null){
+    this.setState({
+      result: n1 - n2,
+      sinal: "-"
+    })
+  } else{
+    this.setState({
+      result: "Voce precisa digitar os dois números!"
+    })
+  }
+}
+
+divide = () => {
+  let {n1, n2} = this.state
+  if(n1 && n2 !== null){
+    this.setState({
+      result: n1 / n2,
+      sinal: "/"
+    })
+  } else{
+    this.setState({
+      result: "Voce precisa digitar os dois números!"
+    })
+  }
+}
+
+multiplica = () => {
+  let {n1, n2} = this.state
+  if(n1 && n2 !== null){
+    this.setState({
+      result: n1 * n2,
+      sinal: "*"
+    })
+  } else{
+    this.setState({
+      result: "Voce precisa digitar os dois números!"
+    })
+  }
+}
+
+clear = () =>{
+  let {n1,n2} = this.state 
+  if (n1 && n2 !== ""){
+    this.setState({
+      n1:0,
+      n2:0,
+      result: "Voce limpou os resultados"
+    })
+  }else{
+    this.setState({
+      result: "Nenhum valor a ser apagado"
+    })
+   
+  }
+}
+
+handleChange = (e) => {
+  let {n1, n2} = this.state
+  this.setState({
+    n1: Number(e.target.value)
+  })
+}
+ 
+handleChange2 = (e) => {
+  let {n1, n2} = this.state
+  this.setState({
+    n2: Number(e.target.value)
+  })
+}
+
+render(){
+  let {add, remove, divide, multiplica, clear,handleChange,handleChange2} = this
+  let {n1, n2, result, sinal} = this.state
+  return(
+    <div>
+      <h1>Calculadora</h1>
+      <h2>{result}</h2>
+      <div className='inputs'>
+        <input onChange={handleChange} type="Number" value={n1}/>
+        <h2>{sinal}</h2>
+        <input onChange={handleChange2} type="Number" value={n2}/>
+      </div>
+      <div className='bottons'>
+        <button onClick={add}> + </button>
+        <button onClick={remove}>-</button>
+        <button onClick={divide}>/</button>
+        <button onClick={multiplica}>*</button>
+        <button onClick={clear}>Clear</button>
+
+
+
+
+      </div>
+    </div>
+  )
+}
+}
